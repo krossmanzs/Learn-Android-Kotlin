4e714ccf86a60046d8d98078dd212e0c9b6c0e43
krossmanzs <radi.adi78@gmail.com>
2022-05-19 14:17:56 +0700

    Learning how Activity Lifecycle works

--- appcomponent/src/main/java/com/krossmanzs/appcomponent/MainActivity.kt
+++ appcomponent/src/main/java/com/krossmanzs/appcomponent/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.krossmanzs.appcomponent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1: Button = findViewById(R.id.btn1)
+
+        btn1.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+        val btnFinish: Button = findViewById(R.id.btnFinish)
+
+        btnFinish.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(applicationContext, "This is onStart()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(applicationContext, "This is onResume()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(applicationContext, "This is onDestroy()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(applicationContext, "This is onPause()", Toast.LENGTH_SHORT).show()
     }
 }
