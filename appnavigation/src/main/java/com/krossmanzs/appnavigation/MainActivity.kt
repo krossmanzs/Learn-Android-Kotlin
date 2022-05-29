e465b6ad0bff6c3187ea30a82f95d8b7f4eaf496
krossmanzs <radi.adi78@gmail.com>
2022-05-29 18:57:57 +0700

    Create button to select Navigation Component or Fragment activities

--- appnavigation/src/main/java/com/krossmanzs/appnavigation/MainActivity.kt
+++ appnavigation/src/main/java/com/krossmanzs/appnavigation/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.krossmanzs.appnavigation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.krossmanzs.appnavigation.navgraph.NavGraphActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnNavGraph : Button = findViewById(R.id.btnNavGraph)
+        val btnFragment : Button = findViewById(R.id.btnFragment)
+
+        btnNavGraph.setOnClickListener {
+            Intent(this, NavGraphActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
---
+++ appnavigation/src/main/java/com/krossmanzs/appnavigation/navgraph/NavGraphActivity.kt
@@ -0,0 +1,12 @@
+package com.krossmanzs.appnavigation.navgraph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.krossmanzs.appnavigation.R
+
+class NavGraphActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_graph)
+    }
+}
