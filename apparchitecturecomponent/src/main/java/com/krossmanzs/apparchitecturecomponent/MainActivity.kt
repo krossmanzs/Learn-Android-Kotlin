58021ca520ffdafe72945204de0c6080e2b6b4b1
krossmanzs <radi.adi78@gmail.com>
2022-06-01 14:55:31 +0700

    Add todo: Learn how to implement Dependency Injection

--- apparchitecturecomponent/src/main/java/com/krossmanzs/apparchitecturecomponent/MainActivity.kt
+++ apparchitecturecomponent/src/main/java/com/krossmanzs/apparchitecturecomponent/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    // TODO: Learn how to implement Dependency Injection 
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
