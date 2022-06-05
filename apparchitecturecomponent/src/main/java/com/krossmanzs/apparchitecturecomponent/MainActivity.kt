a1e21c32db31d753bdd4c0ae7b2a9a81539778b9
krossmanzs <radi.adi78@gmail.com>
2022-06-05 23:50:57 +0700

    Learning how to implement ViewModel and LiveData

--- apparchitecturecomponent/src/main/java/com/krossmanzs/apparchitecturecomponent/MainActivity.kt
+++ apparchitecturecomponent/src/main/java/com/krossmanzs/apparchitecturecomponent/MainActivity.kt
@@ -2,13 +2,34 @@ package com.krossmanzs.apparchitecturecomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.krossmanzs.apparchitecturecomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    // TODO: Learn how to implement Dependency Injection 
-    
+    // learn viewModel and LiveData in kotlin
+    // https://www.youtube.com/watch?v=whAVI1vTOko
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var viewModel : MainActivityViewModel
+
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        viewModel.count.observe(this) {
+            binding.tvCount.text = "$it"
+        }
+
+        binding.btnCount.setOnClickListener {
+            viewModel.updateCount()
+        }
     }
 }
